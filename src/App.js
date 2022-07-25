0cca621596789529a76da9c79aaced3c3682f264
PJMantoss <joelptoss@gmail.com>
2022-07-25 19:50:21 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Wiki Finder</h1>
         <form className='searchBox'>
           <input type='search' placeholder='What are you searching for?' />
