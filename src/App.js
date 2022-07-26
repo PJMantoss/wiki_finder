9707487a26c711786fcbc8c8423af8bb1efd9f2d
PJMantoss <joelptoss@gmail.com>
2022-07-26 16:19:43 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
         <form className='searchBox'>
           <input type='search' placeholder='What are you searching?' />
         </form>
+        <p className='searchResults'>Search: 10</p>
       </header>
+      <div className='results'>
+        <div className='result'>
+          <h3></h3>
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 }
